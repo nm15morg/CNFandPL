04da6a353b2368bb3ff55cd45efde329529ff94a
nm15morg <nm15morg@siena.edu>
2023-04-03 19:25:35 -0400

    Initial commit

--- cnfConverter.java
+++ cnfConverter.java
@@ -1,3 +1,23 @@
+import java.io.File;
+import java.util.Scanner;
 public class cnfConverter{
+    //Replace all biconditionals(a = b) with (a > b) & (b > a)
+    //Replace all implies(a > b) with ~a | b
+    //Apply DeMorgan's Law to move not(~) inwards
+    //Distribute or(|) over and(&)
 
-}
+    public static void main(String[]args) {
+        try {
+            File file = new File("./placeHolder.txt");
+            Scanner sc = new Scanner(file);
+            String input = sc.nextLine();
+            input = input.replaceAll(" ", "");
+            System.out.println(input);
+
+
+            sc.close();
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+} 
