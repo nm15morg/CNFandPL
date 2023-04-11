c24e7bca6b1be0d9bc49161fe8e8d4b609296641
nm15morg <nm15morg@siena.edu>
2023-04-11 10:50:48 -0400

    Tree designed and some functions beginning to be implemented

--- cnfConverter.java
+++ cnfConverter.java
@@ -2,7 +2,6 @@ import java.io.File;
 import java.util.Scanner;
 import java.util.Arrays;
 
-import java.util.Stack;
 public class cnfConverter{
     //Replace all biconditionals(a = b) with (a > b) & (b > a)
     //Replace all implies(a > b) with ~a | b
@@ -14,9 +13,8 @@ public class cnfConverter{
             File file = new File("./cnfExample.txt");
             Scanner sc = new Scanner(file);
             String input = sc.nextLine();
-            input = input.replaceAll(" ", "");
-            char[] inputArray = input.toCharArray();
-            Stack<Character> charStack = new Stack<Character>();
+            Tree myTree = new Tree(input);
+            
 
 
             sc.close();
